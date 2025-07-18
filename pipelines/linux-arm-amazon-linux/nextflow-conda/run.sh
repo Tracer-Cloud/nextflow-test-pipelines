37224f24f26ebe2bff6ad3073b4486a385939d25
mountwo23 <55633208+mountwo23@users.noreply.github.com>
2025-07-18 17:48:04 +0100

    Fix tos

--- pipelines/linux-arm-amazon-linux/nextflow-conda/run.sh
+++ pipelines/linux-arm-amazon-linux/nextflow-conda/run.sh
@@ -33,6 +33,9 @@ fi
 export PATH="$CONDA_HOME/bin:$PATH"
 eval "$("$CONDA_HOME/bin/conda" shell.bash hook)"
 
+conda tos accept --override-channels --channel CHANNEL
+
+
 if conda info --envs | grep -q "^$ENV_NAME "; then
     echo "[INFO] Conda environment '$ENV_NAME' already exists. Skipping creation."
 else
