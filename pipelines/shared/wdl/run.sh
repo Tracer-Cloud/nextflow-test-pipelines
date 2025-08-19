22dbc7ad12a1ce05bcb31e523e26b98541e2dfb7
Vaibhav Upreti <vaibhavupreti@users.noreply.github.com>
2025-08-19 20:45:50 +0100

    chore: update wdl script
    Signed-off-by: Vaibhav Upreti <vaibhavupreti@users.noreply.github.com>

--- pipelines/shared/wdl/run.sh
+++ pipelines/shared/wdl/run.sh
@@ -90,7 +90,7 @@ echo "  - GTF: $GTF"
 echo "  - Test BAM: $TEST_BAM"
 echo "---\n"
 
-pixi run pipeline
+pixi run pipeline_wdl
 
 # REQUIRED=("NA24385_RNAseq_1.fastq.gz" "NA24385_RNAseq_2.fastq.gz" "chr22.fa" "chr22.gtf" "test.bam")
 # for f in "${REQUIRED[@]}"; do
