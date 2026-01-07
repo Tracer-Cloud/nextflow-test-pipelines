3f0b64353bbb9eb496ebb9f2024feb3824515109
Vaibhav Upreti <vaibhav.upreti16@gmail.com>
2026-01-07 21:06:34 +0000

    feat: add airflow docker compose config (#104)

--- pipelines/shared/airflow/dags/pipeline_rnaseq.py
+++ pipelines/shared/airflow/dags/pipeline_rnaseq.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.docker_operator import DockerOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from airflow.operators.python import PythonOperator
 import logging
 
